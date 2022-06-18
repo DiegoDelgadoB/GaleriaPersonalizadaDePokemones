39eb15fabaa2b84f4f1da269051a22e55233a880
Diego Delgado Barra <diego.delgadobarra@outlook.com>
2022-06-18 00:16:31 -0400

    LastCommitFinished

--- index.js
+++ index.js
@@ -29,3 +29,22 @@ pokemonesGet().then((results) => {
     });
 });
 
+http
+    .createServer((req, res) => {
+        if (req.url == '/') {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            fs.readFile('index.html', 'utf-8', (err, html) => {
+                res.end(html);
+            });
+        };
+
+        if (req.url == '/pokemones') {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(pokeArreglo));
+            res.end();
+        };
+
+        
+
+    }).listen(3000, () => console.log('Servidor corriendo'));
+    
